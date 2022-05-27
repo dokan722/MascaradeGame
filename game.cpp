f3c31a7b48d4e105dc2ec68eadf6f0fc6f84080f
Piotrek Matusiak <peiter20000@gmail.com>
2022-05-27 21:43:09 +0200

    crucial functions

--- game.cpp
+++ game.cpp
@@ -5,3 +5,129 @@ GameMaster::GameMaster(QObject *parent):
 {
 
 }
+
+void HumanPlayer::checkSelf()
+{
+    QString role = gameMaster->playerRoles[myId];
+    QString msg = "Twoja rola to: " + role;
+    notify({myId}, msg);
+}
+
+void HumanPlayer::changeRoles(int playerId)
+{
+    QStringList answers = {"Tak", "Nie"};
+    QString answer = saySomething("Czy chcesz zamienić rolę?", answers);
+    if (answer == answers[0])
+        std::swap(gameMaster->playerRoles[myId], gameMaster->playerRoles[playerId]);
+}
+
+void HumanPlayer::changeRoles(int playerId1, int playerId2)
+{
+    QStringList answers = {"Tak", "Nie"};
+    QString answer = saySomething("Czy chcesz zamienić rolę?", answers);
+    if (answer == answers[0])
+        std::swap(gameMaster->playerRoles[playerId1], gameMaster->playerRoles[playerId2]);
+}
+
+QString HumanPlayer::chooseRoleToPlay()
+{
+    QString answer = saySomething("Wybierz rolę ", gameMaster->rolesInPlay);
+    return answer;
+}
+
+bool HumanPlayer::questionRole(QString role)
+{
+    QStringList answers = {"Tak", "Nie"};
+    QString answer = saySomething(
+                "Czy chcesz zakwestionować i ogłosić, że twoja rola to " + role + " ?",
+                answers);
+    if (answer == answers[0])
+        return true;
+    return false;
+}
+
+QVector<int> HumanPlayer::chooseTargets(int numOfTargets)
+{
+    QVector<int> targets;
+    QStringList possibleTargets;
+    for (int i = 0; i < gameMaster->numberOfPlayers; ++i)
+    {
+        if (i != myId)
+            possibleTargets.append(QString::number(i));
+    }
+    for (int i = 0; i < numOfTargets; ++i)
+    {
+        int target = saySomething("Wybierz cel ", possibleTargets).toInt();
+        targets.push_back(target);
+    }
+    return targets;
+}
+
+void HumanPlayer::getOtherRole(int playerId, QString role)
+{
+    notify({playerId}, " rola to " + role);
+}
+
+void HumanPlayer::playerGetsMoney(int playerId, int amount)
+{
+    notify({playerId}, "otrzymuje " + QString::number(amount) + " monet");
+}
+
+void HumanPlayer::potentialExchange(int playerId1, int playerId2)
+{
+    notify({playerId1, playerId2}, "dokonano potencjalnej zamiany");
+}
+
+void HumanPlayer::cashExchange(int playerId1, int playerId2)
+{
+    notify({playerId1, playerId2}, "zamienili się monetami");
+}
+
+void HumanPlayer::informDecision(int playerId, Decision dec)
+{
+    QString decision = "";
+    if (dec == Decision::CHECK)
+        decision = "sprawdzić kartę";
+    else if (dec == Decision::CHANGE)
+        decision = "dokonać potencjalnej zamiany";
+    else if (dec == Decision::USE)
+        decision = "zadeklarować rolę";
+    notify({playerId}, "zdecydował się " + decision);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- game.h
+++ game.h
@@ -15,26 +15,33 @@ enum Decision
 class GameMaster;
 class Player
 {
+protected:
     GameMaster *gameMaster;
+    int myId;
 public:
     Player() {}
-    Player(GameMaster *gm)
+
+    void assign(GameMaster *gm, int id)
     {
         gameMaster = gm;
+        myId = id;
     }
+
     virtual QString saySomething(QString question, QStringList answers) = 0;
     virtual void notify(QVector<int> player, QString card) = 0;
     virtual Decision chooseMove() = 0;
-//    virtual void checkSelf() = 0;
-//    virtual void changeRole(int playerId) = 0;
-//    virtual QString chooseRoleToPlay() = 0;
-//    virtual QString answerWithRole() = 0;
-//    virtual bool questionRole(QString role) = 0;
-//    virtual QVector<int> chooseTargets(int numOfTargets) = 0;
-//    virtual void getOtherRole(int playerId, QString role) = 0;
-//    virtual void playerGetsMoney(int playerId, int amount) = 0;
-//    virtual void potentialExchange(int playerId1, int playerId2) = 0;
-//    virtual void cashExchange(int playerId1, int playerId2) = 0;
+    virtual void checkSelf() = 0;
+    virtual void changeRoles(int playerId) = 0;
+    virtual void changeRoles(int playerId1, int playerId2) = 0;
+    virtual QString chooseRoleToPlay() = 0;
+    virtual QString answerWithRole() = 0;
+    virtual bool questionRole(QString role) = 0;
+    virtual QVector<int> chooseTargets(int numOfTargets) = 0;
+    virtual void getOtherRole(int playerId, QString role) = 0;
+    virtual void playerGetsMoney(int playerId, int amount) = 0;
+    virtual void potentialExchange(int playerId1, int playerId2) = 0;
+    virtual void cashExchange(int playerId1, int playerId2) = 0;
+    virtual void informDecision(int playerId, Decision dec) = 0;
 };
 
 
@@ -51,6 +58,69 @@ public:
     Decision chooseMove() override
     {
         return Decision::CHECK;
+    }
+
+    void checkSelf() override
+    {
+
+    }
+
+    void changeRoles(int playerId) override
+    {
+
+    }
+
+    void changeRoles(int playerId1, int playerId2) override
+    {
+
+    }
+
+    QString chooseRoleToPlay() override
+    {
+        return "Król";
+    }
+
+    QString answerWithRole() override
+    {
+        return "Wieśniak";
+    }
+
+    bool questionRole(QString role) override
+    {
+        return false;
+    }
+
+    QVector<int> chooseTargets(int numOfTargets) override
+    {
+        QVector<int> targets;
+        for (int i = 0; i < numOfTargets; ++i)
+            targets.push_back(0);
+        return targets;
+    }
+
+    void getOtherRole(int playerId, QString role) override
+    {
+
+    }
+
+    void playerGetsMoney(int playerId, int amount) override
+    {
+
+    }
+
+    void potentialExchange(int playerId1, int playerId2) override
+    {
+
+    }
+
+    void cashExchange(int playerId1, int playerId2) override
+    {
+
+    }
+
+    void informDecision(int playerId, Decision dec) override
+    {
+
     }
 };
 
@@ -84,10 +154,10 @@ public:
         return ans;
     }
 
-    void notify(QVector<int> players, QString card) override
+    void notify(QVector<int> players, QString msg) override
     {
         mutex.lock();
-        emit notified(players, card);
+        emit notified(players, msg);
         bed.wait(&mutex);
         mutex.unlock();
     }
@@ -107,6 +177,19 @@ public:
         return Decision::NONE;
     }
 
+    void checkSelf() override;
+    void changeRoles(int playerId) override;
+    void changeRoles(int playerId1, int playerId2) override;
+    QString chooseRoleToPlay() override;
+    QString answerWithRole() override {return chooseRoleToPlay();}
+    bool questionRole(QString role) override;
+    QVector<int> chooseTargets(int numOfTargets) override;
+    void getOtherRole(int playerId, QString role) override;
+    void playerGetsMoney(int playerId, int amount) override;
+    void potentialExchange(int playerId1, int playerId2) override;
+    void cashExchange(int playerId1, int playerId2) override;
+    void informDecision(int playerId, Decision dec) override;
+
 private:
     QMutex mutex;
     QWaitCondition bed;
@@ -123,35 +206,52 @@ class GameMaster: public QThread
 {
     Q_OBJECT
 private:
+
+
+    QVector<QStringList> rolesMatrix = {
+        {"Sędzia", "Biskup", "Król", "Królowa", "Wiedźma", "Oszust"},
+        {"Sędzia", "Biskup", "Król", "Królowa", "Złodziej", "Wiedźma", "Szpieg",},
+        {"Sędzia", "Biskup", "Król", "Błazen", "Królowa", "Wiedźma", "Wieśniak"},
+        {"Sędzia", "Biskup", "Król", "Błazen", "Królowa", "Wiedźma", "Wieśniak", "Oszust"},
+        {"Sędzia", "Biskup", "Król", "Błazen", "Królowa", "Wiedźma", "Szpieg", "Wieśniak", "Oszust"},
+        {"Sędzia", "Biskup", "Król", "Błazen", "Królowa", "Wiedźma", "Szpieg", "Wieśniak", "Oszust", "Inkwizytor"},
+        {"Sędzia", "Biskup", "Król", "Błazen", "Królowa", "Wiedźma", "Szpieg", "Wieśniak", "Oszust", "Inkwizytor", "Wdowa"},
+        {"Sędzia", "Biskup", "Król", "Błazen", "Królowa", "Złodziej", "Wiedźma", "Szpieg", "Wieśniak", "Oszust", "Inkwizytor", "Wdowa"}
+    };
+public:
+    QStringList rolesInPlay;
+    QStringList playerRoles;
     QString difficulty;
     int numberOfPlayers;
-public:
+
     GameMaster(QObject *parent = nullptr);
 
     void setParams(QString diff, int num)
     {
         difficulty = diff;
         numberOfPlayers = num;
+        int row = num - 6;
+        rolesInPlay = rolesMatrix[row];
+        //std::copy(rolesInPlay.begin(), rolesInPlay.end(), playerRoles);
+        playerRoles = rolesInPlay;
+        std::srand ( unsigned ( std::time(0) ) );
+        std::random_shuffle(playerRoles.begin(), playerRoles.end());
     }
 
     void run() override
     {
         int player = 0;
         players.push_back(humanPlayer);
+        humanPlayer->assign(this, 0);
         for (int i = 1; i < numberOfPlayers; ++i)
+        {
             players.push_back(std::make_shared<ComputerPlayer>());
+            players[i]->assign(this, i);
+        }
         while (true)
         {
-            Decision decision = players[player]->chooseMove();
-            if (decision == Decision::CHECK)
-                std::cerr << "Check" << std::endl;
-            else if (decision == Decision::CHANGE)
-                std::cerr << "Change" << std::endl;
-            else if (decision == Decision::USE)
-                std::cerr << "Use" << std::endl;
-            else
-                std::cerr << "Oof" << std::endl;
-            player = (player + 1) % numberOfPlayers;
+            humanPlayer->saySomething("takie role", rolesInPlay);
+            humanPlayer->saySomething("takie role wymieszane", playerRoles);
         }
     }
 
@@ -160,34 +260,52 @@ public:
         return humanPlayer;
     }
 
-    void informAllDecision(Decision decision)
+    void informAllDecision(int playerId, Decision decision, int notTarget = -1)
     {
-
+        for (int i = 0; i < numberOfPlayers; ++i)
+        {
+            if (i != playerId)
+                players[i]->informDecision(playerId, decision);
+        }
     }
 
-    void informAllRole(QString role)
+    void informAllRole(int playerId, QString role)
     {
-
+        for (int i = 0; i < numberOfPlayers; ++i)
+            players[i]->getOtherRole(playerId, role);
     }
 
-    void questionAll(QString role)
+    void questionAll(QString role, int notTarget = -1)
     {
-
+        for (int i = 0; i < numberOfPlayers; ++i)
+        {
+            if (i != notTarget)
+                players[i]->questionRole(role);
+        }
     }
 
     void informAllMoney(int playerId, int amount)
     {
-
+        for (int i = 0; i < numberOfPlayers; ++i)
+            players[i]->playerGetsMoney(playerId, amount);
     }
 
-    void informAllPotentialExchange(int playerId1, int playerId2, int not_target = -1)
+    void informAllPotentialExchange(int playerId1, int playerId2, int notTarget = -1)
     {
-
+        for (int i = 0; i < numberOfPlayers; ++i)
+        {
+            if (i != notTarget)
+                players[i]->potentialExchange(playerId1, playerId2);
+        }
     }
 
-    void informAllCashExchange(int playerId, int playerId2, int not_target = -1)
+    void informAllCashExchange(int playerId1, int playerId2, int notTarget = -1)
     {
-
+        for (int i = 0; i < numberOfPlayers; ++i)
+        {
+            if (i != notTarget)
+                players[i]->cashExchange(playerId1, playerId2);
+        }
     }
 
 private:
